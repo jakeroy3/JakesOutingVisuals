613c0ddc7d9b7ccedf7d2c60d9b9a30fd2949e77
jakeroy3 <114620330+jakeroy3@users.noreply.github.com>
2024-02-09 23:01:02 -0500

    Update AppTest2.py

--- AppTest2.py
+++ AppTest2.py
@@ -100,6 +100,7 @@ def pitch_scatters(card_player, selected_date):
 
     sns.scatterplot(data=amovement, ax=ax, x='horizontal_movement', y='vertical_movement',
                     hue='pitchtype', legend=True, s=150, palette=pitch_palette, marker='*')
+    ax.legend(title='Season AVG Pitch Movement')
     sns.move_legend(ax, "lower center", bbox_to_anchor=(0.5, -.5), ncol=len(amovement), fontsize=10)
     sns.despine(fig)
     st.pyplot(fig)

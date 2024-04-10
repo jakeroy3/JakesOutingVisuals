f478af4b8cd1ffc1b592802bb22ba362da87d833
jakeroy3 <114620330+jakeroy3@users.noreply.github.com>
2024-04-10 10:28:52 -0700

    Add files via upload
    Added 2024 data

--- AppTest2.py
+++ AppTest2.py
@@ -7,20 +7,30 @@ import seaborn as sns
 
 st.title("Jake's Outing Visualizer")
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
-with col3:
-    years = list(('2023', '2022'))
+with col2:
+    years = list(('2024', '2023', '2022'))
     selected_year = st.selectbox('Choose a Year:', years)
 
-pitch_df = pd.read_parquet('AppDataPitches'+selected_year+'.parquet')
+with col2:
+    comparisons = list(('2024', '2023', '2022'))
+    compare = st.selectbox('Compare to:', comparisons)
 
-pitch_list = list(pitch_df['pitchtype'].value_counts().index)
+pitch_df = pd.read_parquet('AppDataPitches'+selected_year+'.parquet')
+pitch_df['game_date'] = pd.to_datetime(pitch_df['game_date'], format='mixed')
+pitch_df['game_date'] = pitch_df['game_date'].dt.date
+
+compare_df = pd.read_parquet('AppDataPitches'+compare+'.parquet')
+compare_df['game_date'] = pd.to_datetime(compare_df['game_date'])
+compare_df['game_date'] = compare_df['game_date'].dt.date
+
+pitch_list = list(pitch_df['pitch_type'].value_counts().index)
 marker_colors = dict(zip(pitch_list,
                          list(sns.color_palette('tab20', n_colors=len(pitch_list)))))
 
-pitcher_list = list(pitch_df.groupby(['name', 'pitchtype'])['pitch_id'].count().reset_index().query('pitch_id >=20')[
-                        'name'].sort_values().unique())
+pitcher_list = list(pitch_df.groupby(['player_name', 'pitch_type'])['pitch_id'].count().reset_index().query('pitch_id>=5')[
+                        'player_name'].sort_values().unique())
 
 
 # col1, col2, col3 = st.columns(3)
@@ -35,11 +45,11 @@ with col1:
     # 'pitchtype'])
     # pitch_type = st.selectbox('Choose a pitch:', pitches)
 
-with col2:
+with col1:
     # Date
-    game_dates = list(pitch_df.loc[pitch_df['name'] == card_player].groupby('game_date').count()
+    game_dates = list(pitch_df.loc[pitch_df['player_name'] == card_player].groupby('game_date').count()
                       .reset_index().query('pitch_id>0')['game_date'])
-    selected_date = st.selectbox('Choose a Date:', game_dates)
+    selected_date = st.selectbox('Game Date:', game_dates)
 
 # Plot parameters
 sz_bot = 1.5
@@ -50,19 +60,21 @@ y_lim = 6
 plate_y = -.25
 
 pitch_palette = {'FF': 'C3', 'FS': 'C5', 'KC': 'C2', 'FC': 'C0',
-                 'ST': 'C4', 'SL': 'C1', 'CH': 'C6', 'SI': 'C9',
-                 'CU': 'C8', 'PO': 'C9', 'KN': 'C7'}
+                 'ST': 'C4', 'SL': 'C1', 'CH': 'C6', 'SI': 'C8',
+                 'CU': 'C9', 'PO': 'C9', 'KN': 'C7', 'FA': 'C10',
+                 'EP': 'C8'}
+
 def pitch_scatters(card_player, selected_date):
     sns.set_theme(style='darkgrid')
-    pitches_scatter = pitch_df.loc[(pitch_df['name'] == card_player) & (pitch_df['game_date'] == selected_date)]
+    pitches_scatter = pitch_df.loc[(pitch_df['player_name'] == card_player) & (pitch_df['game_date'] == selected_date)].query('pitch_id>0')
 
     # Plot Movements
     grid = plt.GridSpec(2, 2, width_ratios=[1, 1], height_ratios=[1, 1])
     # scatter_grid = gridspec.GridSpecFromSubplotSpec(2, 4, subplot_spec=grid[0])
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(grid[0])
-    sns.scatterplot(data=pitches_scatter, x='horizontal_movement', y='vertical_movement',
-                    hue='pitchtype', s=25, color=marker_colors, legend=False, palette=pitch_palette)
+    sns.scatterplot(data=pitches_scatter, x='pfx_x', y='pfx_z',
+                    hue='pitch_type', s=25, color=marker_colors, legend=False, palette=pitch_palette)
 
     ax.axhline(0, color='k', linestyle='--', linewidth=1, alpha=0.5)
     ax.axvline(0, color='k', linestyle='--', linewidth=1, alpha=0.5)
@@ -72,8 +84,8 @@ def pitch_scatters(card_player, selected_date):
 
     # Plot Locations
     ax1 = fig.add_subplot(grid[1])
-    sns.scatterplot(data=pitches_scatter, x='horizontal_location', y='vertical_location',
-                    hue='pitchtype', legend=False, palette=pitch_palette)
+    sns.scatterplot(data=pitches_scatter, x='plate_x', y='plate_z',
+                    hue='pitch_type', legend=False, palette=pitch_palette)
 
     # Strike zone outline
     ax1.plot([-10 / 12, 10 / 12], [sz_bot, sz_bot], color='k', linewidth=2)
@@ -96,60 +108,75 @@ def pitch_scatters(card_player, selected_date):
                 color='k', linewidth=2)
     ax1.plot([8.28 / 12, 0], [plate_y + 0.15, plate_y + 0.25], color='k', linewidth=2)
     ax1.plot([-8.28 / 12, 0], [plate_y + 0.15, plate_y + 0.25], color='k', linewidth=2)
+    ax1.set_xlabel('Horizontal Location')
+    ax1.set_ylabel('Vertical Location')
 
     ax1.set(xlim=(x_ft, -x_ft),
             ylim=(y_bot, y_lim),
             aspect=1)
+    ax1.set_title('Locations ' + str(selected_date) + '\n (Pitcher Perspective)')
 
-    amovement = pitch_df.loc[pitch_df['name'] == card_player]
-    amovement = (amovement.loc[:, ['vertical_movement', 'horizontal_movement', 'name', 'pitchtype']].
-                 groupby(['name', 'pitchtype']).mean())
+    # Create Table for Average Movements
+    amovement = compare_df.loc[compare_df['player_name'] == card_player]
+    amovementcount = amovement.loc[:, ['player_name', 'pitch_id', 'pitch_type']].groupby(['player_name', 'pitch_type'], as_index=False).count()
+    amovementcount = amovementcount.rename(columns={'pitch_id':'count'})
+    amovement = amovement.merge(amovementcount, on=['player_name', 'pitch_type'])
+    amovement = (amovement.loc[amovement['count'] >= 5, ['pfx_z', 'pfx_x', 'player_name', 'pitch_type']].
+                 groupby(['player_name', 'pitch_type']).mean())
 
-    sns.scatterplot(data=amovement, ax=ax, x='horizontal_movement', y='vertical_movement',
-                    hue='pitchtype', legend=True, s=150, palette=pitch_palette, marker='*')
+    # Plot Season Average Movements
+    try:
+        sns.scatterplot(data=amovement, ax=ax, x='pfx_x', y='pfx_z',
+                        hue='pitch_type', legend=True, s=150, palette=pitch_palette, marker='*')
+        ax.set_title('Pitch Movements compared to '+str(compare))
+        ax.legend(title='Season AVG Pitch Movement')
+        ax.set_xlabel('Horizontal Movement')
+        ax.set_ylabel('Vertical Movement')
+        sns.move_legend(ax, "lower center", bbox_to_anchor=(0.5, -.55), ncol=(len(amovement)/2)+1, fontsize=10)
+        sns.despine(fig)
 
-    ax.set_title('Pitch Movements')
-    ax1.set_title('Locations - '+selected_date)
-    ax.legend(title='Season AVG Pitch Movement')
-    sns.move_legend(ax, "lower center", bbox_to_anchor=(0.5, -.5), ncol=len(amovement), fontsize=10)
-    sns.despine(fig)
+    except:
+        errormessage = ('Not enough pitches in '+compare+' for comparison. Please choose another year to see average movements.')
+        st.error(errormessage, icon='🚨')
+
     st.pyplot(fig)
 
     # Returns Total Pitches
-    start_total = pitches_scatter.loc[:, ['pitch_id', 'name']].groupby('name', as_index=False).count()
+    start_total = pitches_scatter.loc[:, ['pitch_id', 'player_name']].groupby('player_name', as_index=False).count()
     start_total = start_total.rename(columns={'pitch_id': 'Total'})
 
     # Returns Pitch Type Pitches
-    start_counts = (pitches_scatter.loc[:, ['pitch_id', 'pitchtype', 'name']]
-                    .groupby(['pitchtype', 'name'], as_index=False).count())
+    start_counts = (pitches_scatter.loc[:, ['pitch_id', 'pitch_type', 'player_name']]
+                    .groupby(['pitch_type', 'player_name'], as_index=False).count())
 
+    # Set Up Streamlit Table
     # Returns Stats by Pitch
-    start_sums = (pitches_scatter.loc[:, ['pitchtype', 'name', 'whiff', 'called_strike', 'in_zone',
+    start_sums = (pitches_scatter.loc[:, ['pitch_type', 'player_name', 'whiff', 'called_strike', 'in_zone',
                                           'contact', 'chase_swing']]
-                    .groupby(['pitchtype', 'name'], as_index=False).sum())
+                    .groupby(['pitch_type', 'player_name'], as_index=False).sum())
 
     # Merges DataFrames
     start_counts = start_total.merge(start_counts)
     start_counts = start_counts.merge(start_sums)
-    start_counts = start_counts.drop(columns=['name'])
+    start_counts = start_counts.drop(columns=['player_name'])
 
     start_counts['Usage%'] = round((start_counts['pitch_id'] / start_counts['Total'] * 100), 2)
     start_counts['CSW%'] = round(((start_counts['whiff'] + start_counts['called_strike']) / start_counts['pitch_id']) * 100, 2)
     start_counts['Zone%'] = round((start_counts['in_zone'] / start_counts['pitch_id']) * 100, 2)
 
     # Returns Average Pitch Specs
-    start_avgs = (pitches_scatter.loc[:, ['velo', 'pitchtype', 'vertical_movement', 'horizontal_movement']]
-                  .groupby('pitchtype', as_index=False).mean())
-    start_avgs['velo'] = round(start_avgs['velo'], 2)
-    start_avgs['horizontal_movement'] = round(start_avgs['horizontal_movement'], 2)
-    start_avgs['vertical_movement'] = round(start_avgs['vertical_movement'], 2)
+    start_avgs = (pitches_scatter.loc[:, ['release_speed', 'pitch_type', 'pfx_z', 'pfx_x']]
+                  .groupby('pitch_type', as_index=False).mean())
+    start_avgs['release_speed'] = round(start_avgs['release_speed'], 2)
+    start_avgs['pfx_x'] = round(start_avgs['pfx_x'], 2)
+    start_avgs['pfx_z'] = round(start_avgs['pfx_z'], 2)
 
-    start_stats = start_counts.merge(start_avgs, on='pitchtype')
-    start_stats = start_stats.rename(columns={'pitchtype': 'Pitch Type', 'pitch_id': '#', 'whiff': 'Whiffs',
-                                              'called_strike': 'CS', 'velo': 'MPH', 'vertical_movement': 'VMov',
-                                              'horizontal_movement': 'HMov', 'contact': 'In Play', 'chase_swing': 'Chases'})
+    start_stats = start_counts.merge(start_avgs, on='pitch_type')
+    start_stats = start_stats.rename(columns={'pitch_type': 'Pitch Type', 'pitch_id': '#', 'whiff': 'Whiffs',
+                                              'called_strike': 'CS', 'release_speed': 'MPH', 'pfx_z': 'VMov',
+                                              'pfx_x': 'HMov', 'contact': 'In Play', 'chase_swing': 'Chases'})
     start_stats = start_stats.drop(columns=['in_zone', 'Total'])
-    st.dataframe(data=start_stats, column_order=('Pitch Type', '#', 'Usage%', 'MPH', 'CS', 'Whiffs', 'CSW%', 'Chases',
+    st.dataframe(data=start_stats, width=1200, column_order=('Pitch Type', '#', 'Usage%', 'MPH', 'CS', 'Whiffs', 'CSW%', 'Chases',
                  'In Play', 'Zone%', 'VMov', 'HMov'), hide_index=True)
 
 
